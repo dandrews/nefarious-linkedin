d117826d76ba3532a61fa651d22662106f5ee266
Corey Prophitt <prophittcorey@users.noreply.github.com>
2017-07-15 14:22:34 -0700

    Cleaned up inject.js

--- extension/src/js/inject.js
+++ extension/src/js/inject.js
@@ -1,3 +1,13 @@
+/* globals chrome, localStorage */
+
+//
+// Fetch the encoded extension data from LinkedIn's local storage and send the
+// encoded data to our Chrome extension. 
+// 
+// Needless to say, this will only work on LinkedIn. This file should be 
+// injected into any LinkedIn page.
+//
+
 chrome.runtime.sendMessage(chrome.runtime.id, {
     encodedData: localStorage.getItem('C_C_M')
 });
